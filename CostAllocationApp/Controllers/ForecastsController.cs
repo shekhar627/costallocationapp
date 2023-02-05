4251b25639d0ca1e29ddc24c6fa8c9976755e778
Sudipto Sarker <sarker420@outlook.com>
2023-02-05 18:36:52 +0600

    localhost change to ip

--- CostAllocationApp/Controllers/ForecastsController.cs
+++ CostAllocationApp/Controllers/ForecastsController.cs
@@ -256,8 +256,8 @@ namespace CostAllocationApp.Controllers
 
             using (var client = new HttpClient())
             {
-                //client.BaseAddress = new Uri("http://198.38.92.119:8081/api/Forecasts?data=" + row + "&year=" + year + "&assignmentId=" + assignmentId + "&allocationId=" + allocationId);
-                client.BaseAddress = new Uri("http://localhost:59198/api/Forecasts?data=" + row + "&year=" + year + "&assignmentId=" + assignmentId+ "&allocationId=" + allocationId);
+                client.BaseAddress = new Uri("http://198.38.92.119:8081/api/Forecasts?data=" + row + "&year=" + year + "&assignmentId=" + assignmentId + "&allocationId=" + allocationId);
+                //client.BaseAddress = new Uri("http://localhost:59198/api/Forecasts?data=" + row + "&year=" + year + "&assignmentId=" + assignmentId+ "&allocationId=" + allocationId);
 
                 //HTTP POST
                 var postTask = client.GetAsync("");
